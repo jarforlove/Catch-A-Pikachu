ae4b49ec3eb7a727d8c6f8d58dda0cfd072d016d
Will Shu <will@Wills-iMac.local>
2017-06-25 20:24:16 +0800

    Make random the generation of x&y position, speed

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
+    // a new Enemy-class object will have a random starting x coordinate
+    this.x = randomInt(-505, -101);
+    // a new Enemy-class object will have be randoms positioned among the three lanes
+    this.y = bugYPositions[randomYPosition()];
+    // a new Enemy-class object will have a random speed
+    this.speed = randomInt(100, 300);
 };
 
 // Update the enemy's position, required method for game
@@ -18,9 +21,11 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x = this.x + this.speed * dt;
-    //This code below help the bug to start over again.
-    if (this.x>=606) {
-      this.x=-101;
+    //This code below help the bug to start over again. Use canvas.width instead of a fixed number incase the width changes
+    if (this.x >= canvas.width) {
+      this.x= randomInt(-505, -101);
+      this.y= bugYPositions[randomYPosition()];
+      this.speed = randomInt(100, 300);
     }
     //Below is the collision detection code. Set the width to 50 so player and bug are not going to collide too soon.
     if (this.x < player.x+50 && this.x+50 > player.x && this.y < player.y+50 && this.y+50>player.y) {
@@ -87,14 +92,23 @@ Player.prototype.render = function () {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var bug1= new Enemy(-101, 65, 100);
-var bug2= new Enemy(-101, 65, 294);
-var bug3= new Enemy(-101, 148, 151);
-var bug4= new Enemy(-101, 148, 275);
-var bug5= new Enemy(-101, 233, 325);
-var bug6= new Enemy(-101, 233, 129);
-var allEnemies = [bug1, bug2, bug3, bug4, bug5, bug6];
+var bugYPositions = [65, 148, 233];
+var allEnemies=[];
+for (var i=0; i<6; i++){
+  allEnemies.push(new Enemy());
+}
 var player = new Player(202, 400);
+
+// This function is used for generating random integers for x postion and speed. It takes in two parameters: minimum and maximum.
+function randomInt(min, max){
+  // Math.floor() returns the largest integer less than or equal to a given number
+  return Math.floor(Math.random()*max+min+1);
+}
+
+// This function is used for randomly returning an integer of 0, 1, or 2, which will be used for picking a random y position
+function randomYPosition(){
+  return Math.floor(Math.random()*3);
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- js/engine.js
+++ js/engine.js
@@ -175,9 +175,10 @@ var Engine = (function(global) {
     ]);
     Resources.onReady(init);
 
-    /* Assign the canvas' context object to the global variable (the window
+    /* Assign the canvas and canvas' context object to the global variable (the window
      * object when run in a browser) so that developers can use it more easily
      * from within their app.js files.
      */
+    global.canvas = canvas;
     global.ctx = ctx;
 })(this);
